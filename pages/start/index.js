97c8422bdbb6e20629b4bd4fe36b72ce77260894
Johann <johannmerck@gmail.com>
2024-09-02 14:34:04 +0200

    minor changes

---
+++ pages/start/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledStartPage = styled.section`
+  background-color: var(--primary-color);
+`;
+
+export default function StartPage() {
+  return <StyledStartPage>Servus</StyledStartPage>;
+}
