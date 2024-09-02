1966edfebe8d8a382660672c359ed158f00e484b
Johann <johannmerck@gmail.com>
2024-09-02 16:06:09 +0200

    yo

--- pages/start/index.js
+++ pages/start/index.js
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 import StyledLink from "@/components/StyledLink/StyledLink";
+import Image from "next/image";
 
 export const StyledStartPage = styled.section`
   background-color: var(--primary-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
 `;
 
 export default function StartPage() {
   return (
     <StyledStartPage>
-      <StyledLink href="/create">Go to Garden / Create Pet </StyledLink>
+      <Image
+        alt="Pet App Logo"
+        src="/images/logo.webp"
+        width={200}
+        height={200}
+      />
+      <StyledLink href="/create">Go to Garden / Create Pet</StyledLink>
     </StyledStartPage>
   );
 }
