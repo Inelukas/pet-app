68d957614a4e9e9e62bc536ce3afa83147499476
Johann <johannmerck@gmail.com>
2024-09-18 18:16:11 +0200

    yo

--- components/Popup/Popup.js
+++ components/Popup/Popup.js
@@ -3,15 +3,17 @@ import styled from "styled-components";
 
 const PopupContainer = styled.div`
   position: fixed;
-  bottom: 20px;
+  bottom: 50px;
   left: 50%;
   transform: translateX(-50%);
   background: var(--signal-gradient);
-
+  width: 150px;
+  height: 50px;
   padding: 10px 20px;
   border-radius: 5px;
   z-index: 1000;
   transition: opacity 0.5s ease-in-out;
+  white-space: nowrap;
 `;
 
 function Popup({ message }) {
